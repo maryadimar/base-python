cdc49ee4e1990b75fe4e579ba2e4cf80cc42e793
maryadi <maryadi071@gmail.com>
2019-12-01 02:51:25 +0700

    cek bilangan prima

--- case_logic/bilanganPrima.py
+++ case_logic/bilanganPrima.py
@@ -1,13 +1,19 @@
-num = int(input("masukan bilangan :"))
+def is_prime(x):
+    prime = "Bilangan Prima"
+    # Mengecek jika x < 2 maka bukan bilangan prima
+    if x < 2:
+        prime = "Bukan bilangan prima"
+    # Mengecek jika x = 2 maka bilangan prima
+    if x == 2:
+        prime = "Bilangan Prima"
+    # Memoduluskan x dengan semua bilangan yang lebih kecil dari x
+    for i in range(2, x):
+        # Jika menghasilkan 0 maka bukan bilangan prima
+        if x % i == 0:
+            prime = "Bukan bilangan prima"
+            break
+    return prime
 
-if num > 1:
-	for i in range(2, num):
-		if (num % i) == 0:
-			print(num, "bukan bilangan prima")
-			print(i, "kali", num//i, "=", num)
-			break
-		else:
-			print(num, "adalah bilangan prima")
-#bil prima kuran atau sama dengan 1
-else:
-	print(num, "bukan bilangan prima")
+
+bilangan = int(input("Masukkan Bilangan : "))
+print(is_prime(bilangan))
